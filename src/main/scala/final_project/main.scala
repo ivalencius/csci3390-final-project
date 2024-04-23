d982995de25150b5a80ccf5e2290e5da2335eb8a
Ilan Valencius <ilanvalencius@gmail.com>
2024-04-23 12:38:34 -0400

    working on GreedyMM

--- src/main/scala/final_project/main.scala
+++ src/main/scala/final_project/main.scala
@@ -15,11 +15,11 @@ object maximal{
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
     Logger.getLogger("org.spark-project").setLevel(Level.WARN)
 
-    def maximumDegree(g: Graph[Int, Int]): Int = {
+    def maximumDegree(g: Graph[Int, String]): Int = {
       return g.degrees.map(_._2).max
     }
 
-    def permute(g: Graph[Int, Int]): Graph[Int, Double] = {
+    def permute(g: Graph[Int, String]): Graph[Int, Double] = {
       // Adds a random Double to each edge attr
       val r = new scala.util.Random
       val added_attr = g.mapEdges(edge => r.nextDouble())
@@ -51,7 +51,7 @@ object maximal{
       return partitioned
     }
 
-    def runPartitions(g: Graph[Int, Double], f: Graph[Int, Double] => Graph[Int, Double]): ??? = {
+    def runPartitions(g: Graph[Int, Double], f: Graph[Int, Double] => RDD[Edge[Boolean]]): Array[RDD[Edge[Boolean]]]= {
       /*
       Run the greedy maximal matching algorithm on each partition
       */
@@ -63,7 +63,7 @@ object maximal{
       return partitionedMatching
     }
 
-    def GreedyMM(g: Graph[Int, Double]): Graph[Int, Double] = {
+    def GreedyMM(g: Graph[Int, Double]): RDD[Edge[Boolean]] = {
       /*
       We must have a way to merge the result of the partitions so just return
       the same graph but with a flag on if the edge should be dropped or not
@@ -71,32 +71,58 @@ object maximal{
       // Sort the order of the edges (pi)
       val sorted_edges = g.edges.sortBy(e => e.attr)
       // Overwrite the edge attribute with a flag (true if in matching, false otherwise)
-      val overwriten_attributes = sorted_edges.map({
+      var overwriten_attributes = sorted_edges.map({
         case Edge(src, dst, attr) => Edge(src, dst, false)
       })
       // #####################################################################
       // Up to here is working perfectly
       // #####################################################################
       // Now run greedy algorithm, if edge is in matching, set flag to true
-      val matching = overwriten_attributes.map({
-        case Edge(src, dst, attr) => {
-          ???
-
+      for (edge <- overwriten_attributes.collect()) {
+        val src = edge.srcId
+        val dst = edge.dstId
+        val attr = edge.attr
+        if (attr == false) {
+          if (!overwriten_attributes.filter(e => e.srcId == src || e.dstId == src || e.srcId == dst || e.dstId == dst).map(e => e.attr).reduce(_ || _)) {
+            // Not matched and none of the neighbors are matched!
+            overwriten_attributes = overwriten_attributes.map(e => if (e.srcId == src && e.dstId == dst) Edge(src, dst, true) else e)
           }
         }
-      })
-      return g
+      }
+      // THIS DOESN'T WORK BECAUSE WE NEED TO UPDATE THE GRAPH INSIDE MAP
+      // val matching = overwriten_attributes.map({
+      //   case Edge(src, dst, attr) => {
+      //     if (attr == true){
+      //       // Edge is already in matching
+      //       Edge(src, dst, true)
+      //     } else if (!overwriten_attributes.filter(e => e.srcId == src || e.dstId == src || e.srcId == dst || e.dstId == dst).map(e => e.attr).reduce(_ || _)) {
+      //       // Edge is not in matching neither are any of it's neighbors
+      //       Edge(src, dst, true)
+      //     } else { 
+      //       Edge(src, dst, false)
+      //     }
+      //     }
+      // })
+      // Get the vertices that are in the matching
+      // val in_matching = overwriten_attributes.filter(e => e.attr == true)
+      return overwriten_attributes
     }
 
     def maximalMatching(g: Graph[Int, Int]): Graph[Int, Int] = {
-      var delta = maximumDegree(g)
-      var matching = g
+      var delta = 2 // random starting value
+      var matching = g.mapEdges(e => "unmatched")
       var rounds = 0
       while(delta != 1){
+        val unmatched = matching.subgraph(epred = e => e.attr == "unmatched")
+        delta = maximumDegree(unmatched)
         rounds += 1
-        println("\tDelta: " + delta)
+        println("\tDelta: " + delta) // random starting value
         val p = math.pow(delta, -0.77)
         val k = math.pow(delta, 0.12).ceil.toInt // Should we overestimate or underestimate k?
+        /*
+        We get all the unmatched edges so we don't iterate over the same edges,
+        and that way we don't need to worry about overwriting the matching graph.
+        */
         /*
         1. Permutation
         Rather than have a separate ordering, we add a random Double to each edge
@@ -105,7 +131,8 @@ object maximal{
         but hopefully this happens rarely enough that it doesn't matter (i.e. 
         we won't have the same Double on the same partition in the same iteration)
         */
-        val pi = permute(g) // working
+        
+        val pi = permute(unmatched) // working
         // 2. Edge-sampling
         val GL = edgeSample(pi, p) // working
         /*
@@ -118,18 +145,35 @@ object maximal{
         // #####################################################################
         // For testing (select first partition)
         val sel = partition.subgraph(vpred = (id, attr) => attr == 0)
-        val greedy = GreedyMM(sel)
+        val greedy_edges = GreedyMM(sel) // Output has a true or false on whether it is in matching
+        for (edge <- greedy_edges.collect()) {
+          val src = edge.srcId
+          val dst = edge.dstId
+          val attr = edge.attr
+          if (attr == true) {
+            matching = matching.mapEdges(e => if ((e.srcId == src && e.dstId == dst) || (e.dstId == src && e.srcId == dst)) "matched" else e.attr)
+          }
+        }
+        // matching.subgraph(epred = e => e.attr == "matched").numEdges
         // #####################################################################
         // 4. Run the greedy maximal matching algorithm on each partition
         val partitionedMatching = runPartitions(partition, GreedyMM)
         // 5. Combine the results of the partitions
-        var matching = ...
+        val all_partitioned_edges = partitionedMatching.reduce(_ union _)
+        // Update the matching
+        for (edge <- all_partitioned_edges.collect()) {
+          val src = edge.srcId
+          val dst = edge.dstId
+          val attr = edge.attr
+          if (attr == true) {
+            matching = matching.mapEdges(e => if ((e.srcId == src && e.dstId == dst) || (e.dstId == src && e.srcId == dst)) "matched" else e.attr)
+          }
+        }
         // 6. Update delta and remove all edges and nodes in the matching from the graph
-        g = g.subgraph(epred = e => ...)
-        g.cache()
-        var delta = maximumDegree(g)
+        matching.cache()
       }
       println("Rounds: " + rounds)
+      return matching.subgraph(epred = e => e.attr == "matched")
     }   
 
     def main(args: Array[String]) {
@@ -154,7 +198,7 @@ object maximal{
     // Get file name for output
     val saveName = "./exports/"+file.split("/").last.replace(".csv", "_output.csv")
     // !!! TESTING !!!
-    val file = "/Users/ilanvalencius/Documents/PhD-courses/2-Large-Scale-Data-Processing/csci3390-final-project/data/log_normal_100.csv"
+    // val file = "/Users/ilanvalencius/Documents/PhD-courses/2-Large-Scale-Data-Processing/csci3390-final-project/data/log_normal_100.csv"
     val edges = sc.textFile(file).map(line => {val x = line.split(","); Edge(x(0).toLong, x(1).toLong , 1)} )
     val g = Graph.fromEdges[Int, Int](edges, 0, edgeStorageLevel = StorageLevel.MEMORY_AND_DISK, vertexStorageLevel = StorageLevel.MEMORY_AND_DISK)
     println("Starting to find the maximal matching")
